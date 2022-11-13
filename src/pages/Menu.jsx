482b1d744a6190ed05ccba1833790b783ab7de0f
koral <koral762@gmail.com>
2022-11-13 14:22:23 +0200

    footer fixed

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,5 @@
 import { MenuPreview } from "../cmps/MenuPreview"
+import { Footer } from '../cmps/Footer';
 
 export const Menu = () => {
 
@@ -6,15 +7,19 @@ export const Menu = () => {
     const objMore = require('../data/more.json');
 
     return (
-        <div className="menu-container">
-            <p className="menu-title">הטעמים המיוחדים שלנו</p>
-            <p className="menu-postage" style={{ fontSize: '18px' }}>דמי משלוח - 15 ש"ח</p>
-            <p className="menu-postage" style={{ marginBottom: '40px'}}>תוספת גלידת שמנת - 7 ש"ח*</p>
-            <p className="menu-postage sale">עכשיו במחירי השקה!!! אל תפספסוו</p>
-            <p className="menu-title knafe">:כנאפה</p>
-            {objKnafe.map((knafa) => <MenuPreview obj={knafa} />)}
-            <p className="menu-title more">:שונות</p>
-            {objMore.map((knafa) => <MenuPreview obj={knafa} />)}
+        <div>
+            <div className="menu-container">
+                <p className="menu-title">הטעמים המיוחדים שלנו</p>
+                <p className="menu-postage" style={{ fontSize: '18px' }}>דמי משלוח - 15 ש"ח</p>
+                <p className="menu-postage" style={{ marginBottom: '40px' }}>תוספת גלידת שמנת - 7 ש"ח*</p>
+                <p className="menu-postage sale">עכשיו במחירי השקה!!! אל תפספסוו</p>
+                <p className="menu-title knafe">:כנאפה</p>
+                {objKnafe.map((knafa) => <MenuPreview obj={knafa} />)}
+                <p className="menu-title more">:שונות</p>
+                {objMore.map((knafa) => <MenuPreview obj={knafa} />)}
+            </div>
+            <Footer />
         </div>
+
     )
 }
